e663c2ec5b4864d55af7e598d3a50f0294eb5ea8
Jusang98 <114963418+Jusang98@users.noreply.github.com>
2022-10-07 12:29:24 +0900

    김상주의 저장소에서 추가!

--- git_test/src/main/java/Main.java
+++ git_test/src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("양현성");
         System.out.println("강명관");
         System.out.println("커밋 푸시 머지 풀 테스트~!");
+        System.out.println("기뮤띠띠 김뮤띠띠!");
     }
 }
