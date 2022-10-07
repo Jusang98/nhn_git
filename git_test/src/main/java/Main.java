05f9030850439f2525d6e9c4044252bf6b5e96b4
Jusang98 <114963418+Jusang98@users.noreply.github.com>
2022-10-07 12:49:01 +0900

    ddd

--- git_test/src/main/java/Main.java
+++ git_test/src/main/java/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("강명관");
         System.out.println("커밋 푸시 머지 풀 테스트~!");
         System.out.println("기뮤띠띠 김뮤띠띠!");
+        System.out.println("기뮤띠띠 김뮤띠띠!");
+        System.out.println("기뮤띠띠 김뮤띠띠!");
     }
 }
